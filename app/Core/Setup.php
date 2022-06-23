688b86323ea71605454d883972218f405d1d6412
Tomasz Guz <tomasz.guz@bold.dev>
2022-06-23 09:59:11 +0200

    Add custom logo to login page

--- app/Core/Setup.php
+++ app/Core/Setup.php
@@ -15,4 +15,24 @@ class Setup
 
         return $mimes;
     }
+
+    /**
+     * @action login_enqueue_scripts
+     */
+    public function setLoginLogo()
+    {
+
+        if (! empty($logo = get_field('site', 'options')['logo'])) {
+            ?>
+                <style type="text/css">
+                    #login h1 a, .login h1 a {
+                        background: url('<?php echo wp_get_attachment_image_url($logo, 'full'); ?>') no-repeat center center / 75%;
+                        width: 100%;
+                        height: 100px;
+                        margin-top: 100px;
+                    }
+                </style>
+            <?php
+        }
+    }
 }
